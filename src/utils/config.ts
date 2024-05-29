9e7d16af1770b3fe1ea962ad4084ef364e4901ce
Eduardo Castro <tec.eduardocastro@gmail.com>
2024-05-29 06:51:27 -0300

    fix: https in swagger

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -6,7 +6,7 @@ const config_env = {
   port: Number(process.env.PORT_API) || 3000,
   hostname: process.env.HOSTNAME_API || "localhost",
   api_url: process.env.API_URL || "http://localhost:3000",
-  jwt_key: process.env.JWT_KEY || ""
+  jwt_key: process.env.JWT_KEY || "s"
 };
 
 export default config_env;
